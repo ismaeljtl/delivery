6989330e70a399b05e1caa72e9512bf85b844c52
ismaeljtl <ismaelteixeira6@gmail.com>
2019-03-14 19:59:33 -0400

    agregando rutas y componente index (menu principal)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { LoginComponent } from './login/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'index', component: IndexComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { User } from '../../classes/user';
 import { LoginService } from '../login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -13,17 +14,20 @@ export class LoginComponent implements OnInit {
   user = new User(null, '', '', null, null);
   mensaje = '';
 
-  constructor(private loginService: LoginService) { }
+  constructor(private loginService: LoginService, private router: Router) { }
 
   ngOnInit() {
   }
 
+  // metodo submit del formulario
   onSubmit() {
+    // al no ser correctos los datos o no tener los permisos necesarios se muestra un error
     if ( this.loginService.login(this.user) === false ) {
       this.mensaje = 'El usuario o clave introducidos son incorrectos. Pruebe nuevamente.';
     } else {
+      // caso contrario se permite la entrada al usuario al sistema
       this.mensaje = '';
-      return true;
+      this.router.navigateByUrl('/index');
     }
   }
 
