6a1e113f081d8d8247c2a5d3f965efb2b71463b0
ismaeljtl <ismaelteixeira6@gmail.com>
2019-03-23 17:52:01 -0400

    logica de agregar categoria lista

--- src/app/menu/menu/menu.component.ts
+++ src/app/menu/menu/menu.component.ts
@@ -8,14 +8,47 @@ import { Location } from '@angular/common';
 })
 export class MenuComponent implements OnInit {
 
+  errorMsg = '';
+
+  categorias = [
+    'pizza',
+    'hamburguesa',
+    'sandwich'
+  ]
+
   constructor(private _location: Location) { }
 
   ngOnInit() {
   }
 
   // Metodo para navegar a la vista anterior
-  back() {
+  public back() {
     this._location.back();
+  }
+
+  // True si el item no existe en el arreglo de categorias; 
+  // False si ya la categoria esta en el array de categorias
+  public revisarCategorias(item: string) {
+    for (let index = 0; index < this.categorias.length; index++) {
+      if (item.toUpperCase() === this.categorias[index].toUpperCase()) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // Metodo para agregar nueva categoria
+  public registrarCategoria(categoria: string) {
+    if (categoria !== '') {
+      if (this.revisarCategorias(categoria)) {
+        this.categorias.push(categoria);
+        $('#exampleModal').modal('hide');
+      } else {
+        this.errorMsg = 'La categoria no puede repetirse.';
+      }
+    } else {
+      this.errorMsg = 'La categoria no puede estar vacia.';
+    }
   }
 
 }
