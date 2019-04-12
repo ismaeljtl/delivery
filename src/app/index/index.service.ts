ea3560c1764d06753870c9651bc90ded539b90b7
ismaeljtl <ismaelteixeira6@gmail.com>
2019-04-12 10:41:52 -0400

    Creando vista de perfil y agregando archivo de servicio para funcionalidades futuras

---
+++ src/app/index/index.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IndexService {
+
+  constructor() { }
+
+  // En este servicio iran los metodos para mostrar la informacion del perfil de usuario en la pagina.
+
+}
